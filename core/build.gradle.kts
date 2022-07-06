c30cd1790c037b2cc1ebed67659c213c0be520d8
changhwan <chang5hustle@gmail.com>
2022-07-07 03:08:59 +0900

    [init] core module build.gradle 내용수정,(#1)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,43 +1,40 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 26
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
+    buildFeatures {
+        dataBinding = true
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    namespace = "com.readme.android.core_ui"
 }
 
 dependencies {
+    implementation(project(":shared"))
+    implementation(project(":navigator"))
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.2")
-    implementation("com.google.android.material:material:1.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(ThirdPartyDependencies.timber)
+
+    // Android Core
+    implementation(AndroidXDependencies.coreKtx)
+    implementation(AndroidXDependencies.appCompat)
+    implementation(AndroidXDependencies.constraintLayout)
+    implementation(AndroidXDependencies.legacy)
+    implementation(AndroidXDependencies.coroutines)
+    coreLibraryDesugaring(AndroidXDependencies.desugarLibrary)
+
+    // Dagger-Hilt
+    implementation(AndroidXDependencies.hilt)
+    kapt(KaptDependencies.hiltCompiler)
+
+    // Material Design
+    implementation(MaterialDesignDependencies.materialDesign)
+
+    // ImageLoading Library
+    implementation(ThirdPartyDependencies.glide)
 }
