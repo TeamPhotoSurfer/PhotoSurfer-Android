49268236a7223d0f14bb65fa29722e8c32b2e633
changhwan <chang5hustle@gmail.com>
2022-07-07 03:04:05 +0900

    [init] app module build.gradle kts 수정 + 내용수정, leakcanary 충돌문제 해결(#1)

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,7 +1,7 @@
 object Constants {
     const val packageName = "com.readme.android"
     const val compileSdk = 32
-    const val minSdk = 23
+    const val minSdk = 26
     const val targetSdk = 32
     const val versionCode = 1
     const val versionName = "1.0"
