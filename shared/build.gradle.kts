f696d4e346c79e4982ffd8a6b5c3235d46289f39
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-12 05:51:21 +0900

    [chore] remove redundant dependency on build.gradle.kts (#12)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -11,17 +11,7 @@ android {
 
 dependencies {
     // Android Core
-    implementation(AndroidXDependencies.coreKtx)
-    implementation(AndroidXDependencies.appCompat)
-    implementation(AndroidXDependencies.constraintLayout)
-    implementation(AndroidXDependencies.legacy)
-    implementation(AndroidXDependencies.coroutines)
     implementation(AndroidXDependencies.splashScreen)
-
-    // Kotlin
-    implementation(KotlinDependencies.kotlin)
-
-    implementation(AndroidXDependencies.coreKtx)
 
     // Material Design
     implementation(MaterialDesignDependencies.materialDesign)
