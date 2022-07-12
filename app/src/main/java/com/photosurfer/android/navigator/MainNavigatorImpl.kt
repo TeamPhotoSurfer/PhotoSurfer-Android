c57925f05e29570be90caf4af91f2b3d546e277f
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-13 03:46:58 +0900

    [feat] add navigateMain Logic on MainNavigator (#14)

--- app/src/main/java/com/photosurfer/android/navigator/MainNavigatorImpl.kt
+++ app/src/main/java/com/photosurfer/android/navigator/MainNavigatorImpl.kt
@@ -1,5 +1,13 @@
 package com.photosurfer.android.navigator
 
+import android.content.Context
+import com.photosurfer.android.MainActivity
 import javax.inject.Inject
 
-class MainNavigatorImpl @Inject constructor() : MainNavigator
+
+class MainNavigatorImpl @Inject constructor() : MainNavigator {
+    override fun navigateMain(context: Context) {
+        context.startActivities<MainActivity>()
+    }
+
+}
