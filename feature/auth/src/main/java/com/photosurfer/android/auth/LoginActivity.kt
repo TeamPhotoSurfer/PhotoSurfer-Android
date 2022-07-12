74cfda98acf1df9b648a34057750f5c4fdcf066c
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-12 17:00:36 +0900

    [ui] set fade out animation on LoginActivity.kt (#14)

--- feature/auth/src/main/java/com/photosurfer/android/auth/LoginActivity.kt
+++ feature/auth/src/main/java/com/photosurfer/android/auth/LoginActivity.kt
@@ -1,18 +1,41 @@
 package com.photosurfer.android.auth
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.photosurfer.android.auth.databinding.ActivityLoginBinding
 import com.photosurfer.android.core.base.BaseActivity
 import com.photosurfer.android.navigator.MainNavigator
+import com.photosurfer.android.shared.R.color
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     @Inject
     lateinit var mainNavigator: MainNavigator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                val fadeOut = ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f)
+                fadeOut.interpolator = LinearInterpolator()
+                fadeOut.duration = 1000L
+                fadeOut.doOnEnd { splashScreenView.remove() }
+                fadeOut.start()
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.window.statusBarColor =
+                ContextCompat.getColor(this, color.login_blue_top)
+        }
     }
 }
