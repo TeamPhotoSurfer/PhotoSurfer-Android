7fa3b678fd53070f3da09ea910e9184cb3c22368
chaeyeong <shimcy2010@gmail.com>
2022-07-15 00:49:37 +0900

    [feat] 팝업 테마 적용 (#29)

--- feature/main/src/main/java/com/photosurfer/android/main/TagFragment.kt
+++ feature/main/src/main/java/com/photosurfer/android/main/TagFragment.kt
@@ -1,13 +1,18 @@
 package com.photosurfer.android.main
 
-import android.nfc.Tag
+import android.R
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
+import android.widget.PopupMenu
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.view.get
 import com.photosurfer.android.core.base.BaseFragment
 import com.photosurfer.android.domain.entity.TagInfo
 import com.photosurfer.android.main.databinding.FragmentTagBinding
+
 
 class TagFragment : BaseFragment<FragmentTagBinding>(R.layout.fragment_tag) {
 
@@ -18,18 +23,51 @@ class TagFragment : BaseFragment<FragmentTagBinding>(R.layout.fragment_tag) {
         initTagAdapter()
 
 
+
     }
 
     private fun initTagAdapter() {
-        tagListAdapter = TagListAdapter()
+        tagListAdapter = TagListAdapter(object : TagListAdapter.OptionsMenuClickListener {
+            override fun onOptionsMenuClicked(position: Int) {
+                performOptionsMenuClick(position)
+            }
+
+        })
         binding.rcvTagList.adapter = tagListAdapter
         tagListAdapter.tagList.addAll(
             mutableListOf(
                 TagInfo(1, "tag1"),
                 TagInfo(1, "tag2"),
-                TagInfo(1, "tag3")
+                TagInfo(1, "tag3"),
+                TagInfo(1, "tag4"),
+                TagInfo(1, "tag5"),
+                TagInfo(1, "tag6"),
             )
         )
         tagListAdapter.notifyDataSetChanged()
+    }
+
+    private fun performOptionsMenuClick(position: Int) {
+
+        val wrapper: Context = ContextThemeWrapper(requireContext(), R.style.popupMenuStyle)
+//        val popup = PopupMenu(wrapper, sourceView)
+
+        val popupMenu = PopupMenu(wrapper, binding.rcvTagList[position].findViewById(R.id.iv_threedots), Gravity.RIGHT)
+        popupMenu.inflate(R.menu.tag_menu)
+        popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener{
+            override fun onMenuItemClick(item: MenuItem?): Boolean {
+                when(item?.itemId) {
+                    R.id.tag_edit -> {
+                        return true
+                    }
+                    R.id.tag_delete -> {
+                        return true
+                    }
+                }
+                return false
+            }
+
+        })
+        popupMenu.show()
     }
 }
--- feature/main/src/main/java/com/photosurfer/android/main/TagListAdapter.kt
+++ feature/main/src/main/java/com/photosurfer/android/main/TagListAdapter.kt
@@ -1,18 +1,26 @@
 package com.photosurfer.android.main
 
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.photosurfer.android.domain.entity.TagInfo
 import com.photosurfer.android.main.databinding.ItemTagBinding
 
 class TagListAdapter(
-
-) : RecyclerView.Adapter<TagListAdapter.TagListViewHolder>() {
+    private var optionsMenuClickListener: OptionsMenuClickListener
+): RecyclerView.Adapter<TagListAdapter.TagListViewHolder>() {
     private var _tagList = mutableListOf<TagInfo>()
     var tagList: MutableList<TagInfo> = _tagList
+
+    interface OptionsMenuClickListener {
+        fun onOptionsMenuClicked(position: Int)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TagListViewHolder {
         val binding: ItemTagBinding = DataBindingUtil.inflate(
@@ -36,12 +44,13 @@ class TagListAdapter(
 
             binding.ivThreedots.setOnClickListener {
                 // 점 3개 누르면 옵션 창 뜨기
-
-
+                optionsMenuClickListener.onOptionsMenuClicked(position)
             }
 
         }
     }
+
+
 
     override fun onBindViewHolder(holder: TagListViewHolder, position: Int) {
         holder.onBind(tagList[position], position)
