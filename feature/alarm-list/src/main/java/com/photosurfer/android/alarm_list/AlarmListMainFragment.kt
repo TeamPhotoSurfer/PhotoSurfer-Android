99984d220a765141298ded880eda7bdc722235d3
changhwan <chang5hustle@gmail.com>
2022-07-16 05:57:17 +0900

    [ui] alarm-tab / ui 작업 상단부 완료 (#45)

---
+++ feature/alarm-list/src/main/java/com/photosurfer/android/alarm_list/AlarmListMainFragment.kt
@@ -0,0 +1,9 @@
+package com.photosurfer.android.alarm_list
+
+import com.photosurfer.android.alarm_list.databinding.FragmentAlarmListMainBinding
+import com.photosurfer.android.core.base.BaseFragment
+
+class AlarmListMainFragment :
+    BaseFragment<FragmentAlarmListMainBinding>(R.layout.fragment_alarm_list_main){
+
+    }
