3255b2384205c6d6d3b6a048db018c7184271ae9
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-17 19:06:20 +0900

    [ui] fragment_setting / xml 작업 (#48)

--- feature/main/src/main/java/com/photosurfer/android/main/setting/SettingFragment.kt
+++ feature/main/src/main/java/com/photosurfer/android/main/setting/SettingFragment.kt
@@ -2,13 +2,30 @@ package com.photosurfer.android.main.setting
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.photosurfer.android.core.base.BaseFragment
 import com.photosurfer.android.main.R
 import com.photosurfer.android.main.databinding.FragmentSettingBinding
+import com.photosurfer.android.main.setting.viewModel.SettingViewModel
+import timber.log.Timber
 
 class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+    private val viewModel: SettingViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        // binding.viewModel = viewModel
+        binding.viewModel = viewModel
+
+        iniClickEvent()
+    }
+
+    private fun iniClickEvent() {
+        with(binding) {
+            clManageAccount.setOnClickListener {
+                Timber.d("setting / 계정관리")
+            }
+            clEtcPolicy.setOnClickListener {
+                Timber.d("setting / 기타정책")
+            }
+        }
     }
 }
