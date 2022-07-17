7983354e64a04edb940586fc81d0dce4fcc4e502
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-17 19:06:04 +0900

    [add] fragment_setting / SettingViewModel.kt 추가 (#48)

---
+++ feature/main/src/main/java/com/photosurfer/android/main/setting/viewModel/SettingViewModel.kt
@@ -0,0 +1,11 @@
+package com.photosurfer.android.main.setting.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import com.photosurfer.android.core.base.BaseViewModel
+
+class SettingViewModel : BaseViewModel() {
+    val userName = "포토서퍼"
+    val email = "photosurfer@gmail.com"
+    val isPushOn: MutableLiveData<Boolean> = MutableLiveData(true)
+
+}
