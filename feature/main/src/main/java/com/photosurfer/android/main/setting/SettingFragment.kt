6d2e3ed025ce12cd00f8670c8b5a38a42614db27
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-17 16:44:58 +0900

    [fix] MainActivity / BottomNavigation ItemClickListener 수정 (#48)

---
+++ feature/main/src/main/java/com/photosurfer/android/main/setting/SettingFragment.kt
@@ -0,0 +1,14 @@
+package com.photosurfer.android.main.setting
+
+import android.os.Bundle
+import android.view.View
+import com.photosurfer.android.core.base.BaseFragment
+import com.photosurfer.android.main.R
+import com.photosurfer.android.main.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // binding.viewModel = viewModel
+    }
+}
