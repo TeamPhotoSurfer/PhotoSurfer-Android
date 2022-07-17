b72e0c613e6dc58ca49d79d11762466120d8fbb4
chaeyeong <shimcy2010@gmail.com>
2022-07-18 04:31:42 +0900

    [feat] 팝업 로직 수정 (#29)

--- feature/main/src/main/java/com/photosurfer/android/main/TagFragment.kt
+++ feature/main/src/main/java/com/photosurfer/android/main/TagFragment.kt
@@ -4,9 +4,11 @@ import android.content.Context
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.style.AlignmentSpan
+import android.util.Log
 import android.view.Gravity
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
 import android.widget.PopupMenu
 import androidx.appcompat.view.ContextThemeWrapper
 import androidx.core.view.get
@@ -22,20 +24,13 @@ class TagFragment : BaseFragment<FragmentTagBinding>(R.layout.fragment_tag) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initTagAdapter()
-
-
-
     }
 
     private fun initTagAdapter() {
-        tagListAdapter = TagListAdapter(object : TagListAdapter.OptionsMenuClickListener {
-            override fun onOptionsMenuClicked(position: Int) {
-                performOptionsMenuClick(position)
-            }
+        tagListAdapter = TagListAdapter(::performOptionsMenuClick)
 
-        })
         binding.rcvTagList.adapter = tagListAdapter
-        tagListAdapter.tagList.addAll(
+        tagListAdapter.submitList(
             mutableListOf(
                 TagInfo(1, "tag1"),
                 TagInfo(1, "tag2"),
@@ -43,20 +38,42 @@ class TagFragment : BaseFragment<FragmentTagBinding>(R.layout.fragment_tag) {
                 TagInfo(1, "tag4tag4tag4tag4tag4tag4tag4tag4tag4tag4tag4tag4tag4tag4"),
                 TagInfo(1, "tag5"),
                 TagInfo(1, "tag66666666"),
+                TagInfo(1, "tag666666667"),
+                TagInfo(1, "tag666666668"),
+                TagInfo(1, "tag666666669"),
+                TagInfo(1, "tag10"),
+                TagInfo(1, "tag11"),
+                TagInfo(1, "tag666666667"),
+                TagInfo(1, "tag666666668"),
+                TagInfo(1, "tag666666669"),
+                TagInfo(1, "tag10"),
+                TagInfo(1, "tag11"),
+                TagInfo(1, "tag666666667"),
+                TagInfo(1, "tag666666668"),
+                TagInfo(1, "tag666666669"),
+                TagInfo(1, "tag10"),
+                TagInfo(1, "tag11"),
+                TagInfo(1, "tag666666667"),
+                TagInfo(1, "tag666666668"),
+                TagInfo(1, "tag666666669"),
+                TagInfo(1, "tag10"),
+                TagInfo(1, "tag11")
             )
         )
-        tagListAdapter.notifyDataSetChanged()
     }
 
-    private fun performOptionsMenuClick(position: Int) {
+    private fun performOptionsMenuClick(position: Int, threeDot: ImageView) {
 
-        val wrapper: Context = ContextThemeWrapper(requireContext(), com.photosurfer.android.shared.R.style.popupMenuStyle)
+        val wrapper: Context = ContextThemeWrapper(
+            requireContext(),
+            com.photosurfer.android.shared.R.style.popupMenuStyle
+        )
 
-        val popupMenu = PopupMenu(wrapper, binding.rcvTagList[position].findViewById(R.id.iv_threedots), Gravity.RIGHT)
+        val popupMenu = PopupMenu(wrapper, threeDot, Gravity.RIGHT)
         popupMenu.inflate(R.menu.tag_menu)
-        popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener{
+        popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
             override fun onMenuItemClick(item: MenuItem?): Boolean {
-                when(item?.itemId) {
+                when (item?.itemId) {
                     R.id.tag_edit -> {
                         return true
                     }
--- feature/main/src/main/java/com/photosurfer/android/main/TagListAdapter.kt
+++ feature/main/src/main/java/com/photosurfer/android/main/TagListAdapter.kt
@@ -1,26 +1,19 @@
 package com.photosurfer.android.main
 
-import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupMenu
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.photosurfer.android.domain.entity.TagInfo
+import com.photosurfer.android.main.TagListAdapter.*
 import com.photosurfer.android.main.databinding.ItemTagBinding
 
 class TagListAdapter(
-    private var optionsMenuClickListener: OptionsMenuClickListener
-): RecyclerView.Adapter<TagListAdapter.TagListViewHolder>() {
-    private var _tagList = mutableListOf<TagInfo>()
-    var tagList: MutableList<TagInfo> = _tagList
-
-    interface OptionsMenuClickListener {
-        fun onOptionsMenuClicked(position: Int)
-    }
+    private val threeDotClickListener: ((Int, ImageView) -> Unit)? = null
+) : ListAdapter<TagInfo, TagListViewHolder>(TagComparator()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TagListViewHolder {
         val binding: ItemTagBinding = DataBindingUtil.inflate(
@@ -32,10 +25,14 @@ class TagListAdapter(
         return TagListViewHolder(binding)
     }
 
-    inner class TagListViewHolder (
+    class TagListViewHolder(
         private val binding: ItemTagBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: TagInfo, position: Int) {
+        fun onBind(
+            data: TagInfo,
+            position: Int,
+            threeDotClickListener: ((Int, ImageView) -> Unit)? = null
+        ) {
             binding.tagInfo = data
 
             binding.ivStar.setOnClickListener {
@@ -43,18 +40,20 @@ class TagListAdapter(
             }
 
             binding.ivThreedots.setOnClickListener {
-                // 점 3개 누르면 옵션 창 뜨기
-                optionsMenuClickListener.onOptionsMenuClicked(position)
+                threeDotClickListener?.invoke(position, binding.ivThreedots)
             }
-
         }
     }
 
-
-
     override fun onBindViewHolder(holder: TagListViewHolder, position: Int) {
-        holder.onBind(tagList[position], position)
+        holder.onBind(getItem(position), position, threeDotClickListener)
     }
 
-    override fun getItemCount(): Int = _tagList.size
+    private class TagComparator : DiffUtil.ItemCallback<TagInfo>() {
+        override fun areItemsTheSame(oldItem: TagInfo, newItem: TagInfo) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: TagInfo, newItem: TagInfo) =
+            oldItem == newItem
+    }
 }
