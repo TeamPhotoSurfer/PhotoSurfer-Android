6051c9cf3df65416f9c771322d0e917f49441232
changhwan <chang5hustle@gmail.com>
2022-07-17 18:06:21 +0900

    [feat] alarm_list / AlarmListMoreActivity 생성, ui 작업 (#50)

---
+++ feature/alarm-list/src/main/java/com/photosurfer/android/alarm_list/AlarmListMoreActivity.kt
@@ -0,0 +1,13 @@
+package com.photosurfer.android.alarm_list
+
+import android.os.Bundle
+import com.photosurfer.android.alarm_list.databinding.ActivityAlarmListMoreBinding
+import com.photosurfer.android.core.base.BaseActivity
+
+class AlarmListMoreActivity :
+    BaseActivity<ActivityAlarmListMoreBinding>(R.layout.activity_alarm_list_more) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
