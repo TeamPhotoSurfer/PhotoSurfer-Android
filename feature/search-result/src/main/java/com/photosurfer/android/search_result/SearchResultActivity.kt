ae6a86cf10788cc8d271472a36fa3e82507e67e4
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-18 23:53:45 +0900

    [feat] activity_search_result / 칩 추가 (#53)

--- feature/search-result/src/main/java/com/photosurfer/android/search_result/SearchResultActivity.kt
+++ feature/search-result/src/main/java/com/photosurfer/android/search_result/SearchResultActivity.kt
@@ -1,15 +1,37 @@
 package com.photosurfer.android.search_result
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.photosurfer.android.core.base.BaseActivity
+import com.photosurfer.android.core.util.ChipUnSelectableUtil
 import com.photosurfer.android.search_result.databinding.ActivitySearchResultBinding
+import com.photosurfer.android.search_result.viewModel.SearchResultViewModel
+import timber.log.Timber
 
 class SearchResultActivity :
     BaseActivity<ActivitySearchResultBinding>(R.layout.activity_search_result) {
-    // private val viewModel : SearchViewModel by viewModels()
+    private val viewModel: SearchResultViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
 
+        setChip()
+    }
+
+    // Selectable Chip 만들면 코드 교체
+    private fun setChip() {
+        val tagList = viewModel.fakeOftenTagList
+        for (element in tagList) {
+            ChipUnSelectableUtil(this).make(
+                binding.cgSearchTag,
+                element.name,
+                ::onClickChip
+            )
+        }
+    }
+
+    private fun onClickChip() {
+        Timber.d("clciked chip")
     }
 }
--- feature/search-result/src/main/java/com/photosurfer/android/search_result/viewModel/SearchResultViewModel.kt
+++ feature/search-result/src/main/java/com/photosurfer/android/search_result/viewModel/SearchResultViewModel.kt
@@ -1,7 +1,25 @@
 package com.photosurfer.android.search_result.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.photosurfer.android.domain.entity.TagInfo
 
 class SearchResultViewModel : ViewModel() {
     val isNoData = true
+
+    val fakeOftenTagList = listOf(
+        TagInfo(0, "포토서퍼"),
+        TagInfo(1, "카페"),
+        TagInfo(2, "생활꿀팁"),
+        TagInfo(3, "위시리스트"),
+        TagInfo(4, "휴학계획"),
+        TagInfo(5, "여행")
+    )
+    val fakeOftenTagLongList = listOf(
+        TagInfo(0, "포토서퍼포토서퍼포토서퍼포토서퍼포토서퍼"),
+        TagInfo(1, "포토서퍼포토서퍼포토서퍼포토서퍼포토서퍼"),
+        TagInfo(2, "포토서퍼포토서퍼포토서퍼포토서퍼포토서퍼"),
+        TagInfo(3, "포토서퍼포토서퍼포토서퍼포토서퍼포토서퍼"),
+        TagInfo(4, "포토서퍼포토서퍼포토서퍼포토서퍼포토서퍼"),
+        TagInfo(5, "포토서퍼포토서퍼포토서퍼포토서퍼포토서퍼")
+    )
 }
