e6ab29c9b1485c935714ac7d895da97e78387495
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-19 01:10:46 +0900

    [add] ThumbnailAdapter.kt (#53)

---
+++ feature/search-result/src/main/java/com/photosurfer/android/search_result/ThumbnailAdapter.kt
@@ -0,0 +1,43 @@
+package com.photosurfer.android.search_result
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.photosurfer.android.domain.entity.ThumbnailInfo
+import com.photosurfer.android.search_result.databinding.ItemPhotoThumbnailBinding
+
+
+class ThumbnailAdapter(
+    private val onItemClick: ((ThumbnailInfo) -> Unit)
+) : ListAdapter<ThumbnailInfo, ThumbnailAdapter.ViewHolder>(ThumbNailComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemPhotoThumbnailBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val thumbnail = getItem(position)
+        holder.bind(thumbnail, onItemClick)
+    }
+
+    class ViewHolder(
+        private val binding: ItemPhotoThumbnailBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(thumbnail: ThumbnailInfo, onItemClick: ((ThumbnailInfo) -> Unit)) {
+            binding.data = thumbnail
+            binding.root.setOnClickListener { onItemClick(thumbnail) }
+        }
+    }
+
+    private class ThumbNailComparator : DiffUtil.ItemCallback<ThumbnailInfo>() {
+        override fun areItemsTheSame(oldItem: ThumbnailInfo, newItem: ThumbnailInfo) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: ThumbnailInfo, newItem: ThumbnailInfo) =
+            oldItem == newItem
+    }
+}
