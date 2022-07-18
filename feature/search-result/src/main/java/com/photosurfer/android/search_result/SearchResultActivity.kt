2d9725ed38a85232b8f326257b84e60c1223ab3d
Kim Hyo Rim <rlagyfla0709@gmail.com>
2022-07-18 23:48:05 +0900

    [add] activity_search_result.xml / 뷰 작업 (#53)

---
+++ feature/search-result/src/main/java/com/photosurfer/android/search_result/SearchResultActivity.kt
@@ -0,0 +1,15 @@
+package com.photosurfer.android.search_result
+
+import android.os.Bundle
+import com.photosurfer.android.core.base.BaseActivity
+import com.photosurfer.android.search_result.databinding.ActivitySearchResultBinding
+
+class SearchResultActivity :
+    BaseActivity<ActivitySearchResultBinding>(R.layout.activity_search_result) {
+    // private val viewModel : SearchViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ feature/search-result/src/main/java/com/photosurfer/android/search_result/viewModel/SearchResultViewModel.kt
@@ -0,0 +1,7 @@
+package com.photosurfer.android.search_result.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class SearchResultViewModel : ViewModel() {
+    val isNoData = true
+}
