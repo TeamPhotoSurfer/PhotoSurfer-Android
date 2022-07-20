d65923bad0235f9153d335016c9303dad78ea485
chaeyeong <shimcy2010@gmail.com>
2022-07-20 23:56:36 +0900

    [feat] 인텐트 필터 파일 uri, file 형식 받아오기

--- feature/register-tag/src/main/java/com/photosurfer/android/register_tag/RegisterTagActivity.kt
+++ feature/register-tag/src/main/java/com/photosurfer/android/register_tag/RegisterTagActivity.kt
@@ -3,38 +3,29 @@ package com.photosurfer.android.register_tag
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import com.photosurfer.android.core.base.BaseActivity
 import com.photosurfer.android.register_tag.databinding.ActivityRegisterTagBinding
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
+import java.io.FileOutputStream
+
 
 class RegisterTagActivity :
     BaseActivity<ActivityRegisterTagBinding>(R.layout.activity_register_tag) {
 
     private lateinit var chooseTagFragment: ChooseTagFragment
+    private lateinit var chooseTagViewModel: ChooseTagViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        chooseTagViewModel = ChooseTagViewModel()
         navigateToChooseTagFragment()
-        val file = getImageFile()
-    }
-
-    private fun getImageFile(): File {
-        val bundle = Bundle()
-        val imageUri: Uri? = intent.getParcelableExtra(Intent.EXTRA_STREAM)
-
-        intent.getStringExtra(Intent.EXTRA_TEXT).toString()
-        intent.getStringExtra(Intent.ACTION_SEND).toString()
-        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-
-        if (imageUri != null) {
-            bundle.putString("image", imageUri.toString())
-        } else {
-            bundle.putString("image", "")
-        }
-
-        return File(imageUri.toString())
     }
 
     private fun navigateToChooseTagFragment() {
