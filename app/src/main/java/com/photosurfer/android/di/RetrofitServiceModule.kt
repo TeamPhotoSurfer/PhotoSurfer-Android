ed5d8aedca6c26dc17257802c5647695cf9df838
changhwan <chang5hustle@gmail.com>
2022-07-20 10:43:31 +0900

    [feat] push-setting / service 까지 완료 (#65)

--- app/src/main/java/com/photosurfer/android/di/RetrofitServiceModule.kt
+++ app/src/main/java/com/photosurfer/android/di/RetrofitServiceModule.kt
@@ -1,16 +1,19 @@
 package com.photosurfer.android.di
 
+import com.photosurfer.android.data.remote.service.PushSettingService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RetrofitServiceModule {
 
-//    예시
-//    @Provides
-//    @Singleton
-//    fun providesNaverBookSearchService(@NaverBookSearchServer retrofit: Retrofit): NaverBookSearchService =
-//        retrofit.create(NaverBookSearchService::class.java)
+    @Provides
+    @Singleton
+    fun providesPushSettingService(retrofit: Retrofit): PushSettingService =
+        retrofit.create(PushSettingService::class.java)
 }
