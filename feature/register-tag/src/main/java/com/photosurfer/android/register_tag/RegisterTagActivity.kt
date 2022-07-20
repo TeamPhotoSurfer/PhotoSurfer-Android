a6303db7ac601f2f68ff4266729983c37aaafb5f
chaeyeong <shimcy2010@gmail.com>
2022-07-20 20:24:46 +0900

    [feat] 캡처 file 생성 (#54)

--- feature/register-tag/src/main/java/com/photosurfer/android/register_tag/RegisterTagActivity.kt
+++ feature/register-tag/src/main/java/com/photosurfer/android/register_tag/RegisterTagActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import com.photosurfer.android.core.base.BaseActivity
 import com.photosurfer.android.register_tag.databinding.ActivityRegisterTagBinding
 import java.io.File
@@ -18,38 +17,24 @@ class RegisterTagActivity :
         super.onCreate(savedInstanceState)
 
         navigateToChooseTagFragment()
-        val uri: Uri? = getImgToUri()
-        getRealPathFromURI(uri)
+        val file = getImageFile()
     }
 
-    private fun getRealPathFromURI(contentUri: Uri?): String? {
-        val proj = arrayOf(MediaStore.Images.Media.DATA)
-        val cursor = contentResolver.query(contentUri!!, proj, null, null, null)
-        cursor!!.moveToNext()
-        val path = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA))
-        val uri = Uri.fromFile(File(path))
-        Log.d("uri to file : ", "$uri")
-        cursor.close()
-        return path
-    }
-
-    private fun getImgToUri(): Uri? {
-        intent.getStringExtra(Intent.EXTRA_TEXT).toString()
-        intent.getStringExtra(Intent.ACTION_SEND).toString()
-
-        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+    private fun getImageFile(): File {
         val bundle = Bundle()
         val imageUri: Uri? = intent.getParcelableExtra(Intent.EXTRA_STREAM)
+
+        intent.getStringExtra(Intent.EXTRA_TEXT).toString()
+        intent.getStringExtra(Intent.ACTION_SEND).toString()
+        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
         if (imageUri != null) {
             bundle.putString("image", imageUri.toString())
         } else {
             bundle.putString("image", "")
         }
-        Log.d("uri 가져오", "${imageUri}")
 
-        val file = File(imageUri.toString())
-        Log.d("file path... ", "$file")
-        return imageUri
+        return File(imageUri.toString())
     }
 
     private fun navigateToChooseTagFragment() {
