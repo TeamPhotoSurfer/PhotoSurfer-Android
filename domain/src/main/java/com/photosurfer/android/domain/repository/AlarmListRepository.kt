f2b282a7c98cc265eb486ebc7b5a65fc9b5786b9
changhwan <chang5hustle@gmail.com>
2022-07-21 15:03:09 +0900

    [feat] alarm_main / repository 까지 완료 (#77)

--- domain/src/main/java/com/photosurfer/android/domain/repository/AlarmListRepository.kt
+++ domain/src/main/java/com/photosurfer/android/domain/repository/AlarmListRepository.kt
@@ -1,7 +1,12 @@
 package com.photosurfer.android.domain.repository
 
+import com.photosurfer.android.domain.entity.AlarmElement
 import com.photosurfer.android.domain.entity.AlarmInfo
 
 interface AlarmListRepository {
     suspend fun getUrgentAlarmList(): Result<AlarmInfo>
+
+    suspend fun getPassedAlarmList(): Result<List<AlarmElement>>
+
+    suspend fun getUpComingAlarmList(): Result<List<AlarmElement>>
 }
