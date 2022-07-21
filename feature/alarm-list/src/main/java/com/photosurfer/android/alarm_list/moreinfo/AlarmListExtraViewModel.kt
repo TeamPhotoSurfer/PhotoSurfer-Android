9e0815226c6b945b645a949c2cf2508b780ff788
changhwan <chang5hustle@gmail.com>
2022-07-21 15:25:35 +0900

    [feat] alarm_main / viewModel Logic 까지 완료  dataclass 수정해줘야함 (#77)

--- feature/alarm-list/src/main/java/com/photosurfer/android/alarm_list/moreinfo/AlarmListExtraViewModel.kt
+++ feature/alarm-list/src/main/java/com/photosurfer/android/alarm_list/moreinfo/AlarmListExtraViewModel.kt
@@ -2,14 +2,36 @@ package com.photosurfer.android.alarm_list.moreinfo
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.photosurfer.android.core.base.BaseViewModel
 import com.photosurfer.android.domain.entity.AlarmElement
+import com.photosurfer.android.domain.repository.AlarmListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class AlarmListExtraViewModel @Inject constructor() : BaseViewModel() {
+class AlarmListExtraViewModel @Inject constructor(
+    private val alarmListRepository: AlarmListRepository
+) : BaseViewModel() {
 
     private val _alarmData = MutableLiveData<List<AlarmElement>>()
     val alarmData: LiveData<List<AlarmElement>> = _alarmData
+
+    fun getPassedAlarmList() {
+        viewModelScope.launch {
+            alarmListRepository.getPassedAlarmList()
+                .onSuccess { _alarmData.value = it }
+                .onFailure { Timber.d(it, "${this.javaClass.name}_getPassedAlarmList") }
+        }
+    }
+
+    fun getUpComingAlarmList() {
+        viewModelScope.launch {
+            alarmListRepository.getUpComingAlarmList()
+                .onSuccess { _alarmData.value = it }
+                .onFailure { Timber.d(it, "${this.javaClass.name}_getUpComingAlarmList") }
+        }
+    }
 }
