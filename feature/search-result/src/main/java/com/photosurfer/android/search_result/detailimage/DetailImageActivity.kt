6b3a4541024fcbd83d8f391712b20f07994f7683
changhwan <chang5hustle@gmail.com>
2022-07-22 06:44:48 +0900

    [feat] detail_image / activity 메인부분 ui 생성(#90)

---
+++ feature/search-result/src/main/java/com/photosurfer/android/search_result/detailimage/DetailImageActivity.kt
@@ -0,0 +1,13 @@
+package com.photosurfer.android.search_result.detailimage
+
+import android.os.Bundle
+import com.photosurfer.android.core.base.BaseActivity
+import com.photosurfer.android.search_result.R
+import com.photosurfer.android.search_result.databinding.ActivityDetailImageBinding
+
+class DetailImageActivity :
+    BaseActivity<ActivityDetailImageBinding>(R.layout.activity_detail_image) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
