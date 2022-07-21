f6fcdea13ea5c198af9ec0deeb8d444bdd13f3b6
changhwan <chang5hustle@gmail.com>
2022-07-21 14:28:21 +0900

    [feat] alarm_main / dataSource까지 완료 (#77)

--- data/src/main/java/com/photosurfer/android/data/remote/datasource/RemoteAlarmListDataSource.kt
+++ data/src/main/java/com/photosurfer/android/data/remote/datasource/RemoteAlarmListDataSource.kt
@@ -2,9 +2,14 @@ package com.photosurfer.android.data.remote.datasource
 
 import com.photosurfer.android.data.remote.calladapter.NetworkState
 import com.photosurfer.android.data.remote.model.response.BaseResponse
+import com.photosurfer.android.data.remote.model.response.Push
 import com.photosurfer.android.data.remote.model.response.UrgentAlarmListResponse
 
 interface RemoteAlarmListDataSource {
 
     suspend fun getUrgentAlarmList(): NetworkState<BaseResponse<UrgentAlarmListResponse>>
+
+    suspend fun getPassedAlarmList(): NetworkState<BaseResponse<List<Push>>>
+
+    suspend fun getUpComingAlarmList(): NetworkState<BaseResponse<List<Push>>>
 }
--- data/src/main/java/com/photosurfer/android/data/remote/datasource/RemoteAlarmListDataSourceImpl.kt
+++ data/src/main/java/com/photosurfer/android/data/remote/datasource/RemoteAlarmListDataSourceImpl.kt
@@ -2,13 +2,24 @@ package com.photosurfer.android.data.remote.datasource
 
 import com.photosurfer.android.data.remote.calladapter.NetworkState
 import com.photosurfer.android.data.remote.model.response.BaseResponse
+import com.photosurfer.android.data.remote.model.response.Push
 import com.photosurfer.android.data.remote.model.response.UrgentAlarmListResponse
+import com.photosurfer.android.data.remote.service.PassedAlarmListService
+import com.photosurfer.android.data.remote.service.UpComingAlarmListService
 import com.photosurfer.android.data.remote.service.UrgentAlarmListService
 import javax.inject.Inject
 
 class RemoteAlarmListDataSourceImpl @Inject constructor(
-    private val urgentAlarmListService: UrgentAlarmListService
+    private val urgentAlarmListService: UrgentAlarmListService,
+    private val passedAlarmListService: PassedAlarmListService,
+    private val upComingAlarmListService: UpComingAlarmListService
 ) : RemoteAlarmListDataSource {
     override suspend fun getUrgentAlarmList(): NetworkState<BaseResponse<UrgentAlarmListResponse>> =
         urgentAlarmListService.getUrgentAlarmList()
+
+    override suspend fun getPassedAlarmList(): NetworkState<BaseResponse<List<Push>>> =
+        passedAlarmListService.getPassedAlarmList()
+
+    override suspend fun getUpComingAlarmList(): NetworkState<BaseResponse<List<Push>>> =
+        upComingAlarmListService.getUpComingAlarmList()
 }
