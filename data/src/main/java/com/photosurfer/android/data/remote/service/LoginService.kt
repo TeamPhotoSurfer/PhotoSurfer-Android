edb7f902222aef13e6d0274b97fd93bc879fdda9
changhwan <chang5hustle@gmail.com>
2022-08-10 16:25:05 +0900

    [feat] LoginService 까지 생성 (#109)

--- data/src/main/java/com/photosurfer/android/data/remote/service/LoginService.kt
+++ data/src/main/java/com/photosurfer/android/data/remote/service/LoginService.kt
@@ -2,13 +2,15 @@ package com.photosurfer.android.data.remote.service
 
 import com.photosurfer.android.data.remote.calladapter.NetworkState
 import com.photosurfer.android.data.remote.model.request.LoginRequest
+import com.photosurfer.android.data.remote.model.response.BaseResponse
+import com.photosurfer.android.data.remote.model.response.LoginResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface LoginService {
 
-//    @POST("auth/login")
-//    suspend fun postLogin (
-//        @Body body: LoginRequest
-//    ) :NetworkState<>
+    @POST("auth/login")
+    suspend fun postLogin(
+        @Body body: LoginRequest
+    ): NetworkState<BaseResponse<LoginResponse>>
 }
